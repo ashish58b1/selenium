f098a8b0f92c7eca90e3b6d1c53ec2579c150d38
ashish.srivastava <ashish.srivastava@ASHISH-S>
2017-02-07 12:02:12 +0530

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // IWebDriver driver = new FirefoxDriver();
+           // driver.Url = "https://fv3test.azurewebsites.net/login";
+            System.Console.WriteLine("selenium");
+        }
+    }
+}
---
+++ FlowView3/SeleniumTest.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace FlowView3
+{
+    class SeleniumTest
+    {
+        static void Main(string[]args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://fv3test.azurewebsites.net/login";
+            System.Console.WriteLine("selenium");
+            
+        }
+        
+    }
+
+}
